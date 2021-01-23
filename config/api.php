3c77548bd121c16097e845a06abe7d004ad06064
MattX23 <matt.wood001@gmail.com>
2021-01-23 14:18:18 +0000

    change xapi host

--- config/api.php
+++ config/api.php
@@ -3,6 +3,6 @@
 return [
     'corona-api-settings' => [
         'x-rapidapi-key'  => env('X_RAPID_API_KEY'),
-        'x-rapidapi-host' => 'https://covid-193.p.rapidapi.com',
+        'x-rapidapi-host' => 'covid-193.p.rapidapi.com',
     ],
 ];
