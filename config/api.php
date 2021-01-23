821b8ab3b267cba599477745cb922f745cae1baf
MattX23 <matt.wood001@gmail.com>
2021-01-23 14:06:25 +0000

    update config

--- config/api.php
+++ config/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Config;
-
 return [
     'corona-api-settings' => [
         'x-rapidapi-key'  => env('X_RAPID_API_KEY'),
-        'x-rapidapi-host' => 'covid-193.p.rapidapi.com',
+        'x-rapidapi-host' => 'https://covid-193.p.rapidapi.com',
     ],
 ];
